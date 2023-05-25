7ff3d820a1996e99bb46ad2d336ae24b566c5d43
Christian Garcia <bones192nd@gmail.com>
2023-05-24 20:50:47 -0400

    weird lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0: 
+        print([])
+        return
+    elif length == 1: 
+        print([0])
+        return
+
+    test = [0,1]
+
+    for x in range (0, length-2):
+        test.append(test[x+1]+test[x])
+
+    print(test)
+
+
+print_fibonacci(9)
